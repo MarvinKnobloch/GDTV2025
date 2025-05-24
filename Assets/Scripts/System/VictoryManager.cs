d41ff11f9a046e27d1ef4d29c4bae12d87356241
ThunderComplex <thunder.complex@mailbox.org>
2025-05-24 20:35:38 +0200

    small victory improvement

--- Assets/Scripts/System/VictoryManager.cs
+++ Assets/Scripts/System/VictoryManager.cs
@@ -8,17 +8,7 @@ public class VictoryManager : MonoBehaviour
     public Image OverlayImage;
     public float FadeInDuration = 1f;
 
-    private bool _showVictoryScreen = false;
     private float _fadeTimer = 0f;
-
-    public void ShowVictoryScreen()
-    {
-        GameManager.Instance.playerUI.ToggleBossHealth(false);
-        PoolingSystem.PurgePool();
-
-        VictoryCanvas.enabled = true;
-        _showVictoryScreen = true;
-    }
 
     void PostTransition()
     {
@@ -30,19 +20,15 @@ public class VictoryManager : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (_showVictoryScreen)
-        {
-            _fadeTimer += Time.fixedDeltaTime;
-            float alpha = Mathf.Clamp01(_fadeTimer / FadeInDuration);
-            var color = OverlayImage.color;
-            color.a = alpha;
-            OverlayImage.color = color;
+        _fadeTimer += Time.fixedDeltaTime;
+        float alpha = Mathf.Clamp01(_fadeTimer / FadeInDuration);
+        var color = OverlayImage.color;
+        color.a = alpha;
+        OverlayImage.color = color;
 
-            if (alpha >= 1f)
-            {
-                _showVictoryScreen = false;
-                PostTransition();
-            }
+        if (alpha >= 1f)
+        {
+            PostTransition();
         }
     }
 }
