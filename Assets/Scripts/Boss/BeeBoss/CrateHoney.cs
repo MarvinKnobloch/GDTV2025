f25238350f573ca6977fdc664af84a1a833dfb60
MarvinKnobloch <NivramKnobloch@gmail.com>
2025-05-25 15:16:20 +0200

    finished boss2 art

--- Assets/Scripts/Boss/BeeBoss/CrateHoney.cs
+++ Assets/Scripts/Boss/BeeBoss/CrateHoney.cs
@@ -5,11 +5,15 @@ public class CrateHoney : MonoBehaviour
 {
 
     [SerializeField] private GameObject honeyDrop;
+    [SerializeField] private Transform spawnPosition;
+    [SerializeField] private SpriteRenderer defaultSpriteRenderer;
     //Animations
     [NonSerialized] public Animator currentAnimator;
     [NonSerialized] public string currentstate;
     const string idleState = "Idle";
     const string crateState = "Crate";
+
+
 
     private void Awake()
     {
@@ -22,7 +26,7 @@ public class CrateHoney : MonoBehaviour
     }
     public void SpawnDrop()
     {
-        GameObject prefab = Instantiate(honeyDrop, transform.GetChild(0).transform.position, Quaternion.identity);
+        GameObject prefab = Instantiate(honeyDrop, spawnPosition.position, Quaternion.identity);
 
         prefab.transform.Rotate(0, 0, 0);
         //prefab.transform.right = transform.right;
@@ -35,5 +39,10 @@ public class CrateHoney : MonoBehaviour
         if (currentAnimator == null) return;
 
         currentAnimator.CrossFadeInFixedTime(newstate, 0.1f);
+    }
+    public void ChangeSprite()
+    {
+        GetComponent<SpriteRenderer>().enabled = true;
+        defaultSpriteRenderer.enabled = false;
     }
 }
