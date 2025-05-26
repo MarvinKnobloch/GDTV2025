6c2256d28e2839dea26be3c41402b4c15eb6979b
ThunderComplex <thunder.complex@mailbox.org>
2025-05-26 21:43:37 +0200

    fixed head tail attack animation sync

--- Assets/Scripts/Boss/CaterpillarRework/CaterpillarPhase.cs
+++ Assets/Scripts/Boss/CaterpillarRework/CaterpillarPhase.cs
@@ -3,43 +3,45 @@ using UnityEngine;
 
 public class CaterpillarPhase : MonoBehaviour
 {
-	public CaterpillarPhaseSO[] phases;
+    public CaterpillarPhaseSO[] phases;
 
     public Transform HeadPosition;
     public Transform TailPosition;
 
-	public AbstractAttack attacker;
+    public AbstractAttack attacker;
 
     public Animator HeadAnimator { private get; set; }
     public Animator TailAnimator { private get; set; }
 
-	private int phaseIndex = 0;
+    private int phaseIndex = 0;
 
     public IEnumerator Attack(CaterpillarManager manager, bool headAttacksFromLeft, bool tailAttacksFromLeft)
     {
-		var phase = phases[phaseIndex];
+        var phase = phases[phaseIndex];
 
         for (int i = 0; i < phase.HeadAttacks.Length; i++)
         {
-            HeadAnimator.SetTrigger("Shoot");
+            HeadAnimator.SetBool("IsShooting", true);
             TailAnimator.SetBool("IsShooting", true);
 
-			StopAllCoroutines();
-            StartAttack(HeadPosition, headAttacksFromLeft, phase.HeadAttacks[i]);
-            yield return StartAttack(TailPosition, tailAttacksFromLeft, phase.TailAttacks[i]);
+            StopAllCoroutines();
+            var firstAttack = StartAttack(HeadPosition, headAttacksFromLeft, phase.HeadAttacks[i]);
+            var secondAttack = StartAttack(TailPosition, tailAttacksFromLeft, phase.TailAttacks[i]);
 
+            yield return firstAttack;
             HeadAnimator.SetBool("IsShooting", false);
+            yield return secondAttack;
             TailAnimator.SetBool("IsShooting", false);
 
             yield return new WaitForSeconds(phase.TimeBetweenAttacks);
         }
 
-		PhaseTransition(manager);
+        PhaseTransition(manager);
     }
 
-	private void PhaseTransition(CaterpillarManager manager)
+    private void PhaseTransition(CaterpillarManager manager)
     {
-		var transitionAtHealth = Mathf.RoundToInt(phases[phaseIndex].TransitionAtHealth * (PlayerPrefs.GetFloat("BossHealthMultiplier") * 0.01f));
+        var transitionAtHealth = Mathf.RoundToInt(phases[phaseIndex].TransitionAtHealth * (PlayerPrefs.GetFloat("BossHealthMultiplier") * 0.01f));
         if (manager.Health.Value <= transitionAtHealth)
         {
             StopAllCoroutines();
@@ -47,8 +49,8 @@ public class CaterpillarPhase : MonoBehaviour
         }
     }
 
-	private Coroutine StartAttack(Transform origin, bool attackFromLeft, AttackPattern attackPattern)
-	{
-		return StartCoroutine(attacker.ShootProjectiles(origin, attackFromLeft, attackPattern));
-	}
+    private Coroutine StartAttack(Transform origin, bool attackFromLeft, AttackPattern attackPattern)
+    {
+        return StartCoroutine(attacker.ShootProjectiles(origin, attackFromLeft, attackPattern));
+    }
 }
